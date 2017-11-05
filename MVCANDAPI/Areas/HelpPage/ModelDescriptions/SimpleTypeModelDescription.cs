0f84214d95003bde852ce6da3c5f573f93a651b5
Ali <Ali@DESKTOP-N48NU4B>
2017-11-05 17:41:40 -0500

    Add project files.

---
+++ MVCANDAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MVCANDAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ MVCANDAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCANDAPI.WebApiApplication" Language="C#" %>
